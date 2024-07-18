5434715b480408e4046ab4349c435d40191bbdff
Tasogare_no_Jack <jacopoalberti363@gmail.com>
2024-07-18 15:23:23 +0200

    Finished project

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    [SerializeField] private Vector2 m_InitialForce = new Vector2(5, 2);
+    [SerializeField] private Vector2 m_InitialForce = new Vector2(8, 2);
     private Rigidbody2D m_Rigidbody;
 
-    void Start()
+    public void StartBouncing()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
         m_Rigidbody.AddForce(m_InitialForce, ForceMode2D.Impulse);
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,6 +7,7 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI m_HostScoreUI;
     [SerializeField] private TextMeshProUGUI m_ClientScoreUI;
+    [SerializeField] private BallMovement m_Ball;
 
     private int m_HostScore = 0;
     private int m_ClientScore = 0;
@@ -24,5 +25,10 @@ public class UIManager : MonoBehaviour
             m_ClientScore++;
             m_ClientScoreUI.text = m_ClientScore.ToString();
         }
+    }
+
+    public void StartGame()
+    {
+        m_Ball.StartBouncing();
     }
 }
